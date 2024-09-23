63e661c0f64dd6ba667f21b7b23293e903f2f5f3
huytq@ics-p.vn <huytq@ics-p.vn>
2024-09-23 13:49:58 +0700

    add product-service

---
+++ product-service/src/main/java/com/microservice/product_service/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.microservice.product_service.service;
+
+import com.microservice.product_service.dto.ProductRequest;
+import com.microservice.product_service.dto.ProductResponse;
+import com.microservice.product_service.model.Product;
+import com.microservice.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest request) {
+        Product product = Product.builder()
+                .name(request.name())
+                .description(request.description())
+                .price(request.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        var products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+}
